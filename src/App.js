7025a82111422fd36ae3ccb656d02d8850d91aae
Ansonsd5 <anson11sd@gmail.com>
2023-10-17 00:48:28 +0530

    store fix

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Header from './components/Header';
 import Body from './components/Body';
 import store from './utils/store';
 import { Provider } from 'react-redux';
+
 import './App.css';
 
 function App() {
   return (
-   <Provider  store ={store}>
+  <Provider store={store}>
       <Header />
       <Body />
       </Provider>
