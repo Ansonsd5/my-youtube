db15bc07d2a297af12e57aab14c5946327d018c2
Ansonsd5 <anson11sd@gmail.com>
2023-11-30 23:43:28 +0530

    fix: styles changes

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -2,20 +2,29 @@ import React, { useState } from "react";
 
 const Demo = () => {
   const [darkMode, setDarkMode] = useState(true);
-  const [text,setText] = useState('');
+  const [text, setText] = useState("");
 
-  const isPrime = (num)=>{
-    
-  }
+  const isPrime = (num) => {};
   return (
-  
-      <div className={darkMode ? "bg-black w-full max-h-full":"bg-white w-full max-h-full"}>
+    <div
+      className={
+        darkMode ? "bg-black w-full max-h-full" : "bg-white w-full max-h-full"
+      }
+    >
       <section className="border border-blue-700">
-        <button role="btn" className={darkMode ?"text-white": "text-black" } onClick={() => setDarkMode(!darkMode)}>
-         
+        <button
+          role="btn"
+          className= { darkMode ? "text-white" : "text-black"}
+          onClick={() => setDarkMode(!darkMode)}
+        >
           {darkMode ? "lightmode" : "darkmode"}
         </button>
-        <input type="text" value={text} className="bg-green-400 p-2" onChange={(e)=>setText(e.target.value)}/>
+        <input
+          type="text"
+          value={text}
+          className="bg-green-400 p-2 h-[200px] w-[200px]"
+          onChange={(e) => setText(e.target.value)}
+        />
       </section>
     </div>
   );
